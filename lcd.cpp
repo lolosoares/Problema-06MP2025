a490bb9ff0103d13d7a0569b84c26d22cb9bef6a
Robson Soares <robsonnnsoares1@gmail.com>
2025-05-18 09:09:00 +0200

    Create lcd.cpp
    Codigo so com lcd

---
+++ lcd.cpp
@@ -0,0 +1,25 @@
+//C++ code
+#include <LiquidCrystal.h>
+
+// LCD nos pinos: RS, E, D4, D5, D6, D7
+LiquidCrystal lcd(12, 11, A4, A3, A2, A1);
+
+void setup() {
+  lcd.begin(16, 2);  // Inicializa LCD com 16 colunas e 2 linhas
+
+  String dadoInterno = "BEM-VINDO AOS CONTROLADORES";
+  lcd.setCursor(0, 0);
+  lcd.print("Projeto LCD");
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);              // Linha 0, coluna 0
+  lcd.print("BEM-VINDO AOS");
+
+  lcd.setCursor(0, 1);              // Linha 1, coluna 0
+  lcd.print("CONTROLADORES");
+}
+
+void loop() {
+  // Nada acontece no loop
+}
