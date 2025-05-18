5df6a605f40a4e94234f79a756c8bb114eee5df4
Robson Soares <robsonnnsoares1@gmail.com>
2025-05-18 13:26:35 +0200

    Update lcd.cpp
    dado1 dado2

--- lcd.cpp
+++ lcd.cpp
@@ -7,17 +7,18 @@ LiquidCrystal lcd(12, 11, A4, A3, A2, A1);
 void setup() {
   lcd.begin(16, 2);  // Inicializa LCD com 16 colunas e 2 linhas
 
-  String dadoInterno = "BEM-VINDO AOS CONTROLADORES";
+  String dadoInterno1 = "BEM-VINDO AOS";
+  String dadoInterno2 ="CONTROLADORES";
   lcd.setCursor(0, 0);
   lcd.print("Projeto LCD");
   delay(2000);
 
   lcd.clear();
   lcd.setCursor(0, 0);              // Linha 0, coluna 0
-  lcd.print("BEM-VINDO AOS");
+  lcd.print(dadoInterno1);
 
   lcd.setCursor(0, 1);              // Linha 1, coluna 0
-  lcd.print("CONTROLADORES");
+  lcd.print(dadoInterno2);
 }
 
 void loop() {
